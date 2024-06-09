031d3958822cd868757f34caf7d131be23752348
yottoon <sasakiyoshito599@gmail.com>
2024-06-09 15:35:18 +0900

    カレンダーコンポーネントの追加

---
+++ frontend/src/components/calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+
+export default function App() {
+    return (
+    <SafeAreaView>
+        <Calendar />
+        </SafeAreaView>
+        )
+    }
