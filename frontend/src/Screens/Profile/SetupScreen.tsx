7b50cf819c1b22cf77b40dab3f59bd0caaa4433e
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-07-27 16:36:54 +0900

    add: React Native Paperのセットアップ

--- frontend/src/Screens/Profile/SetupScreen.tsx
+++ frontend/src/Screens/Profile/SetupScreen.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { TextInput } from 'react-native-paper';
 import Header from '../../Elements/Header';
 
 function SetupScreen () {
@@ -13,9 +15,35 @@ function SetupScreen () {
 	return (
 	  <View style={{ flex: 1 }}>
       <Header title="初期登録" />
-      {/* 画面実装 */}
+      <PaperProvider>
+        <TextInput
+          mode="outlined"
+          label="ユーザ名"
+          placeholder="ユーザ名を入力してください"
+          value={userName} 
+          onChangeText={setUserName}
+          style={styles.textInput}
+        />
+        <TextInput
+          mode="outlined"
+          label="備考"
+          placeholder="備考を入力してください"
+          value={remarks}
+          onChangeText={setRemarks}
+          style={styles.textInput}
+        />
+      </PaperProvider>
 	  </View>
 	);
 };
+
+const styles = StyleSheet.create({
+	textInput: {
+		justifyContent: 'center', 
+    marginTop: 10,
+    marginHorizontal: 20,
+    borderColor: '#4B8687'
+	},
+});
 
 export default SetupScreen
