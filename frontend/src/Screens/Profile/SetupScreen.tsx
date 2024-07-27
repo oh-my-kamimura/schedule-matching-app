61c89d3a110e693d3affa2b8288e187ba95dc4fa
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-07-27 17:12:25 +0900

    add: テキストボックスとボタンの追加

--- frontend/src/Screens/Profile/SetupScreen.tsx
+++ frontend/src/Screens/Profile/SetupScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { TextInput } from 'react-native-paper';
+import { TextInput, Button } from 'react-native-paper';
 import Header from '../../Elements/Header';
 
 function SetupScreen () {
+  const [userID, setUserID] = useState('');
   const [userName, setUserName] = useState('');
   const [remarks, setRemarks] = useState('');
   const [image, setImage] = useState(null);
@@ -18,8 +19,18 @@ function SetupScreen () {
       <PaperProvider>
         <TextInput
           mode="outlined"
+          label="ユーザID"
+          placeholder="ユーザIDを入力してください"
+          activeOutlineColor='#4B8687'
+          value={userID} 
+          onChangeText={setUserID}
+          style={styles.textInput}
+        />
+        <TextInput
+          mode="outlined"
           label="ユーザ名"
           placeholder="ユーザ名を入力してください"
+          activeOutlineColor='#4B8687'
           value={userName} 
           onChangeText={setUserName}
           style={styles.textInput}
@@ -28,10 +39,18 @@ function SetupScreen () {
           mode="outlined"
           label="備考"
           placeholder="備考を入力してください"
+          activeOutlineColor='#4B8687'
           value={remarks}
           onChangeText={setRemarks}
           style={styles.textInput}
         />
+        <Button 
+          mode="contained" 
+          onPress={() => console.log('Pressed')}
+          style={styles.button}
+        >
+          登録する
+        </Button>
       </PaperProvider>
 	  </View>
 	);
@@ -42,8 +61,15 @@ const styles = StyleSheet.create({
 		justifyContent: 'center', 
     marginTop: 10,
     marginHorizontal: 20,
-    borderColor: '#4B8687'
+    
 	},
+  button: {  
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#4B8687',
+    marginTop: 30,
+    width: 200,
+  }
 });
 
 export default SetupScreen
