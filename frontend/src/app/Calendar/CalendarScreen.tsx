2ec9a2619ac48bbeedee6929b6a767a496d25061
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-09-27 01:54:54 +0900

    add: フッターを追加

--- frontend/src/app/Calendar/CalendarScreen.tsx
+++ frontend/src/app/Calendar/CalendarScreen.tsx
@@ -1,10 +1,6 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { Link } from 'expo-router';
 import Header from '../../Elements/Header';
-
-type RootStackParamList = {
-	Calendar: undefined;
-	Detail: undefined;
-};
 
 function CalendarScreen() {
 	return (
@@ -12,10 +8,9 @@ function CalendarScreen() {
 			<Header title="カレンダー" />
 			<View style={styles.container}>
 				<Text>カレンダー画面です。</Text>
-				<Button
-					title="詳細へ"
-					// onPress={() => navigation.navigate('Detail')}
-				/>
+				<Link href='/Calendar/DetailScreen'>
+					<Button title="詳細へ" />
+				</Link>
 			</View>
 		</View>
 	)
