be2dc98c95bf06752e729360dbe8df7ffbbf1207
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-09-30 23:15:40 +0900

    add: ログイン状態の監視

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,9 +1,21 @@
 import { View, Text } from 'react-native';
-import { Redirect } from 'expo-router';
+import { Redirect, router } from 'expo-router';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+
+import { auth } from '../config';
 
 function Index () {
+	useEffect(() =>
+		onAuthStateChanged(auth, (user) => {
+			if(user !== null) {
+				router.replace('/Calendar/CalendarScreen')
+			}
+		})
+	, [])
+
 	return (
-		<Redirect href='Auth/SignUpScreen' />
+		<Redirect href='Auth/LogInScreen' />
 	)
 }
 
