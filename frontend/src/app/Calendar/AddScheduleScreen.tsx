13fd2873287aa5472a45db2ca50e9447a51d13ff
yottoon <sasakiyoshito599@gmail.com>
2024-10-06 17:34:38 +0900

    update : スケジュール追加画面

--- frontend/src/app/Calendar/AddScheduleScreen.tsx
+++ frontend/src/app/Calendar/AddScheduleScreen.tsx
@@ -1,11 +1,55 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Header from '../../Elements/Header';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { TextInput, Button } from 'react-native-paper';
+import { useState } from 'react';
 
 function AddScheduleScreen() {
+	const [title, setTitle] = useState('');
+	const [date, setDate] = useState(new Date());
+	const [open, setOpen] = useState(false);
+	const [memo, setMemo] = useState('');
+
+
+
+	const handleRegister = (): void => {
+
+	};
+
 	return (
 		<View style={{ flex: 1 }}>
 			<Header title="予定追加ページ" />
 			<View style={styles.container}>
+				<TextInput
+				label="タイトル"
+				mode="outlined"
+				style={styles.textInput}
+				activeOutlineColor='#4B8687'
+				value={title}
+				onChangeText={setTitle}
+				placeholder="予定のタイトルを入力してください"
+				placeholderTextColor='#AAAAAA'
+				keyboardType='default'
+				autoCapitalize='none'
+				/>
+				<TextInput
+				label="メモ"
+				mode="outlined"
+				style={styles.memoInput}
+				activeOutlineColor='#4B8687'
+				value={memo}
+				onChangeText={setMemo}
+				placeholderTextColor='#AAAAAA'
+				keyboardType='default'
+				autoCapitalize='none'
+				/>
+				<Button
+				mode="contained"
+				onPress={() => { handleRegister() }}
+				style={styles.button}
+				>
+				追加する
+				</Button>
 			</View>
 		</View>
 	)
@@ -13,19 +57,26 @@ function AddScheduleScreen() {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: '#ddd',
-		alignItems: 'center',
-		justifyContent: 'center',
+		flex: 1
 	},
-	input: {
-		borderColor: 'gray',
-        borderWidth: 1,
-        borderRadius: 5,
-        height: 40,
-        width: 200,
-        fontSize: 20,
+	textInput: {
+		justifyContent: 'center',
+		marginTop: 10,
+		marginHorizontal: 20,
+	},
+	memoInput: {
+		justifyContent: 'center',
+		marginTop: 10,
+		marginHorizontal: 20,
+		height: 200,
+	},
+	button: {
+		justifyContent: 'center',
+		alignSelf: 'center',
+		backgroundColor: '#4B8687',
+		marginTop: 30,
+		width: 200,
 	},
 });
 
-export default AddScheduleScreen
+export default AddScheduleScreen;
