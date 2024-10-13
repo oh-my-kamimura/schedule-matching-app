bcc38578d2c0d7eccf9deb923b6df5444f4a064d
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-10-13 22:06:01 +0900

    add: storageに画像がアップロードされるように修正

--- frontend/src/Components/UploadProfileImage.tsx
+++ frontend/src/Components/UploadProfileImage.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import { Image, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import * as ImagePicker from 'expo-image-picker';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import uuid from 'react-native-uuid';
+
+import { storage } from '../config';
 
 function UploadProfileImage() {
 
@@ -10,8 +14,8 @@ function UploadProfileImage() {
 	const pickImageAndUpload = async () => {
 		const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 		if (permissionResult.granted === false) {
-		  alert("画像を選択するための権限がありません。");
-		  return;
+			alert("画像を選択するための権限がありません。");
+			return;
 		}
 
 		const result = await ImagePicker.launchImageLibraryAsync({
@@ -23,22 +27,37 @@ function UploadProfileImage() {
 
 		if (!result.canceled) {
 			setSelectedImage(result.assets[0].uri);
-			console.log(result.assets[0].uri);
+			
+			const uploadImage = async (uri: string) => {
+				try {
+					const response = await fetch(uri);
+					const blob = await response.blob();
+					const filename = uuid.v4();
+					const storageRef = ref(storage, `profile_images/${filename}`);
+					await uploadBytes(storageRef, blob);
+					const downloadURL = await getDownloadURL(storageRef);
+					console.log('Uploaded image URL:', downloadURL);
+				} catch (error) {
+					console.error("画像のアップロードに失敗しました:", error);
+				}
+			};
+			
+			uploadImage(result.assets[0].uri);
 		}
 	};
-
+	
 	return (
-		<TouchableOpacity onPress={() => pickImageAndUpload() }>
+		<TouchableOpacity onPress={() => pickImageAndUpload()}>
 			{selectedImage ? (
-				<Image 
+				<Image
 					source={{ uri: selectedImage }}
-					style={styles.image} 
+					style={styles.image}
 					width={100}
 				/>
 			) : (
 				<Icon
-					name="person-circle" 
-					color={"#666666"} 
+					name="person-circle"
+					color={"#666666"}
 					size={100}
 				/>
 			)}
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
@@ -17,5 +18,6 @@ const auth = initializeAuth(app, {
 	persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 })
 const db = getFirestore(app)
+const storage = getStorage(app)
 
-export { app, auth, db }
+export { app, auth, db, storage }
