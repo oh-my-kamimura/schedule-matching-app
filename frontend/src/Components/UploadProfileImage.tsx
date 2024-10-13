6b54d7a58eea804c3912b86866d6ab91392c28d5
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-10-14 00:34:54 +0900

    add: Cloud storageに画像を保存

--- frontend/src/Components/UploadProfileImage.tsx
+++ frontend/src/Components/UploadProfileImage.tsx
@@ -2,14 +2,15 @@ import { useState } from 'react';
 import { Image, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import * as ImagePicker from 'expo-image-picker';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
 import uuid from 'react-native-uuid';
 
 import { storage } from '../config';
 
 function UploadProfileImage() {
 
-	const [selectedImage, setSelectedImage] = useState<string | null>(null);
+	const [selectedImage, setSelectedImage] = useState<string>('');
+	const [selectedImageFilepath, setSelectedImageFilepath] = useState<string>('');
 
 	const pickImageAndUpload = async () => {
 		const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -26,26 +27,35 @@ function UploadProfileImage() {
 		});
 
 		if (!result.canceled) {
+			if (selectedImageFilepath !== '') {
+				deleteImage();
+			}
 			setSelectedImage(result.assets[0].uri);
-			
-			const uploadImage = async (uri: string) => {
-				try {
-					const response = await fetch(uri);
-					const blob = await response.blob();
-					const filename = uuid.v4();
-					const storageRef = ref(storage, `profile_images/${filename}`);
-					await uploadBytes(storageRef, blob);
-					const downloadURL = await getDownloadURL(storageRef);
-					console.log('Uploaded image URL:', downloadURL);
-				} catch (error) {
-					console.error("画像のアップロードに失敗しました:", error);
-				}
-			};
-			
 			uploadImage(result.assets[0].uri);
+		};
+	}
+	
+	const deleteImage = async () => {
+		const storageRef = ref(storage, selectedImageFilepath);
+		await deleteObject(storageRef);
+		console.log('古い画像を削除しました。');
+	}
+
+	const uploadImage = async (uri: string) => {
+		try {
+			const response = await fetch(uri);
+			const blob = await response.blob();
+			const filename = uuid.v4();
+			const storageRef = ref(storage, `profile_images/${filename}`);
+			await uploadBytes(storageRef, blob);
+			const downloadURL = await getDownloadURL(storageRef);
+			console.log('Uploaded image URL:', downloadURL);
+			setSelectedImageFilepath(`profile_images/${filename}`);
+		} catch (error) {
+			console.error("画像のアップロードに失敗しました:", error);
 		}
 	};
-	
+
 	return (
 		<TouchableOpacity onPress={() => pickImageAndUpload()}>
 			{selectedImage ? (
